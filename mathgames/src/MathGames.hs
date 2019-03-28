34182e881b8c473a15a1392c957007971129c885
Ivan Murashko <ivan.murashko@gmail.com>
2019-03-28 16:21:06 +0300

    haskell: MathGames added

--- mathgames/src/MathGames.hs
+++ mathgames/src/MathGames.hs
@@ -1,7 +1,14 @@
-module MathGames ( allbeginnum ) where
+module MathGames ( allbeginnum,  isgoodnumber) where
 
 -- Begin number is a number gotten by removing all last digests --
 allbeginnum :: Int -> [Int]
 allbeginnum x = if x < 10 
                 then [] 
                 else let y = x `div` 10 in y : (allbeginnum y)
+
+-- Is the digit devided by all begin numbers
+isgoodnumber :: Int -> Bool
+isgoodnumber x = all (\t -> x `mod` t == 0) $ allbeginnum x
+
+-- Max 6 digit number that divided by all it begins number
+max6digit = undefined
