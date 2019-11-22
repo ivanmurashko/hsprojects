b01ca34b9a614e23f851d254152f50e398321a06
Ivan Murashko <ivan.murashko@gmail.com>
2019-11-22 18:04:56 +0300

    codewars: The Supermarket Queue tests

--- codewars/test/Spec.hs
+++ codewars/test/Spec.hs
@@ -1,2 +1,16 @@
+import Test.HUnit
+import Test.Framework
+import Test.Framework.Providers.HUnit
+import TheSupermarketQueue
+
+testTheSupermarketQueue :: Assertion
+testTheSupermarketQueue = map (\(xs,n) -> queueTime xs n) 
+                          [([], 1), ([1,2,3,4], 1), 
+                           ([2,2,3,3,4,4], 2), ([1,2,3,4,5], 100)] @?= 
+                          [0, 10, 9, 5]
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMainWithOpts
+       [testCase "The Supermarket Queue" testTheSupermarketQueue
+       ]
+       mempty   
