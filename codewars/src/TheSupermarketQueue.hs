071657663f4b29a8b6fc3f4ada0eed25bce5efab
Ivan Murashko <ivan.murashko@gmail.com>
2019-11-22 14:19:52 +0300

    codewars: The Supermarket Queue stub

---
+++ codewars/src/TheSupermarketQueue.hs
@@ -0,0 +1,7 @@
+-- https://www.codewars.com/kata/the-supermarket-queue/train/haskell
+module TheSupermarketQueue
+    ( queueTime
+    ) where
+
+queueTime :: [Int] -> Int -> Int
+queueTime = undefined
