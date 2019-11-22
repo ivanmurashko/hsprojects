54a2671d967ad38bcad4f5374760a1ea1c7b7275
Ivan Murashko <ivan.murashko@gmail.com>
2019-11-22 13:39:49 +0300

    stacktest: quickcheck for min element in the list

--- stacktest/src/Lib.hs
+++ stacktest/src/Lib.hs
@@ -1,8 +1,16 @@
 module Lib
-    ( fib
+    ( fib, minlist
     ) where
 
 fib :: Word -> Integer
 fib n =  fst $ helper n (0, 1) where 
     helper 0 (x, y) = (x, y)
     helper n (x, y) = helper (n-1) (y, x + y)
+
+minlist :: [Int] -> Maybe Int
+minlist [] = Nothing
+minlist [x] = Just x
+minlist (x:xs) = minlist xs >>= \y -> Just (min x y)
+
+queueTime :: [Int] -> Int -> Int
+queueTime = undefined
--- stacktest/test/Spec.hs
+++ stacktest/test/Spec.hs
@@ -7,5 +7,25 @@ prop_fib n
     | n == 1 = fib n == 1
     | otherwise = (fib n + fib (n+1)) == fib (n + 2) 
 
+-- The element is less all others
+prop_minlist1 :: [Int] -> Bool
+prop_minlist1 xs = filter ( \x -> minlist_less x (minlist xs) ) xs == [] 
+    where 
+      minlist_less _ Nothing = False
+      minlist_less x (Just y) = x < y
+
+-- The element is in the lsit
+prop_minlist2 :: [Int] -> Bool
+prop_minlist2 xs
+    | xs == [] = minlist xs == Nothing
+    | otherwise = 
+        (length $ filter ( \x -> minlist_eq x (minlist xs) ) xs) > 0 where
+            minlist_eq _ Nothing = False
+            minlist_eq x (Just y) = x == y
+
+
+
 main :: IO ()
-main = quickCheck prop_fib
+main = quickCheck prop_fib >> 
+       quickCheck prop_minlist1 >> 
+       quickCheck prop_minlist2
