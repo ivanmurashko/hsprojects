e0df29c5db4fc2dd689b590e9ed9c760e378008c
Ivan Murashko <ivan.murashko@gmail.com>
2019-12-06 13:57:42 +0300

    mathexperiments: tests

--- mathexperiments/test/Spec.hs
+++ mathexperiments/test/Spec.hs
@@ -1,23 +1,49 @@
-import Test.HUnit
-import Test.Framework
-import Test.Framework.Providers.HUnit
+import Test.Tasty
+import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck
 import Data.List
 import MathGames
 
-testAllBeginNumbers :: Assertion
-testAllBeginNumbers = sort (allbeginnum 12345) @?= [1,12,123,1234]
+-- Fibonachi numbers tests
+prop_fib :: Word -> Bool
+prop_fib n
+    | n == 0 = fib n == 0
+    | n == 1 = fib n == 1
+    | otherwise = (fib n + fib (n+1)) == fib (n + 2) 
 
-testFibonachi :: Assertion
-testFibonachi = map fib [0,1,2,3,4,5] @?= [0,1,1,2,3,5]
+testFib :: TestTree
+testFib = testGroup "Fibonachi numbers tests" [
+           testCase "Fibonachi base" $ map fib [0,1,2,3,4,5] @?= [0,1,1,2,3,5], 
+           testProperty "Base property" prop_fib
+          ]
 
-testFactorial :: Assertion
-testFactorial = map fac [0,1,2,3,4,5] @?= [1,1,2,6,24,120]
 
+-- Lucas numbers
+prop_luk :: Word -> Bool
+prop_luk n
+    | n == 0 = luk n == 2
+    | n == 1 = luk n == 1
+    | otherwise = (luk n + luk (n+1)) == luk (n + 2) 
+
+testLuk :: TestTree
+testLuk = testGroup "Luckas numbers tests" [
+           testProperty "Base property" prop_luk
+          ]
+
+-- Misc tests
+testMisc :: TestTree
+testMisc = testGroup "Misc tests" [
+            testCase "Factorial tests" $
+                     map fac [0,1,2,3,4,5] @?= [1,1,2,6,24,120], 
+            testCase "All begin numbers" $ 
+                     sort (allbeginnum 12345) @?= [1,12,123,1234]
+           ]
+
+
+allTests :: TestTree
+allTests = testGroup "Math experiments tests" [
+            testFib, testLuk, testMisc
+           ]
 
 main :: IO ()
-main = defaultMainWithOpts
-       [testCase "All begin numbers" testAllBeginNumbers,
-        testCase "Fibonachi" testFibonachi,
-        testCase "Factorial" testFactorial
-       ]
-       mempty   
+main = defaultMain allTests
